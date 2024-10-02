30c106cf11c5f55f0148beaca8f8da2703c854e3
Micah Raji <micahraji@gmail.com>
2024-10-02 21:04:02 +0100

    I am learning JavaScript

---
+++ Interactive.js
@@ -0,0 +1,37 @@
+//Boolean has two values that can either be true or false
+Boolean = true 
+console.log("Micah loves Football");
+
+const Num1 = 208;
+
+const Num2 = 349;
+
+console.log(Num1 + Num2)
+
+
+const Num3 = 930;
+
+const Num4 = 33;
+
+console.log(Num3 / Num4)
+
+console.log(Num1 - Num3)
+
+console.log(Num2 * Num4)
+
+const name = "Micah"
+console.log(name[3])
+
+const name2 = "Jerry"
+console.log(name2[4])
+
+const firstname = "Peter"
+console.log(firstname)
+
+var lastname = "Dennis"
+
+lastname = "Parker"
+console.log(lastname)
+
+console.log(firstname + lastname)
+
